bc73cb13e089186038a258546898e065969fb125
Alone <tranluong460@gmail.com>
2023-11-20 20:25:23 +0700

    feat: up api admin room type refs:DTN-55

---
+++ FE/src/components/admin/RoomType/CreateRoomTypeModal.tsx
@@ -0,0 +1,70 @@
+import { Button, Modal, Form, Input, Space, message } from "antd";
+import { useCreateRoomTypeMutation } from "../../../api/roomType";
+
+type CreateRoomTypeModalProps = { isOpenCreate: boolean; onCancel: () => void };
+
+const CreateRoomTypeModal = ({
+  isOpenCreate,
+  onCancel,
+}: CreateRoomTypeModalProps) => {
+  const [form] = Form.useForm();
+  const [createRoomType, resultCreate] = useCreateRoomTypeMutation();
+
+  const onFinish = (data: {
+    name: string;
+    features?: [{ name: string; surcharge: boolean }];
+  }) => {
+    createRoomType(data)
+      .unwrap()
+      .then((response) => {
+        message.success(response.message);
+        form.resetFields();
+        onCancel();
+      })
+      .catch((error) => {
+        message.error(error.data.errors);
+      });
+  };
+
+  return (
+    <Modal
+      title="Thêm mới loại phòng"
+      open={isOpenCreate}
+      onCancel={onCancel}
+      footer={null}
+    >
+      <Form
+        name="add_room_type"
+        form={form}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="name"
+          label="Tên loại phòng"
+          rules={[{ required: true, message: "Vui lòng nhập tên loại phòng!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <Space>
+            <Button htmlType="submit" loading={resultCreate.isLoading}>
+              Thêm
+            </Button>
+
+            <Button
+              htmlType="reset"
+              onClick={onCancel}
+              disabled={resultCreate.isLoading}
+            >
+              Hủy
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreateRoomTypeModal;
