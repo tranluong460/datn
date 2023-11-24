0b55d96bb1054f968af1f62cc65a63accdbfbb54
phucquyet1202 <phucquyet1222003@gmail.com>
2023-11-24 10:58:05 +0700

    feat:[BE] Booking refs: DTN-23

--- BE/src/controller/payment.js
+++ BE/src/controller/payment.js
@@ -2,85 +2,119 @@ import { sendResponse } from "../utils";
 import { PaymentModel } from "../models";
 import { PaymentValidate } from "../validate";
 import { validateMiddleware } from "../middleware";
+import axios from "axios";
+import crypto from 'crypto';
+import config from 'config';
+import qs from 'qs';
+import dateFormat from 'dateformat';
 
 
+/**
+ * 
+ * Hàm createPaymentUrl trong đoạn mã của bạn có nhiệm vụ tạo URL thanh toán 
+ * và chuyển hướng người dùng đến cổng thanh toán VNPAY để thực hiện thanh toán
+ */
+export const createPaymentUrl = (req, res) => {
+  const ipAddr =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
 
-export const create = async (req, res) => {
-  try {
-    validateMiddleware(req, res, PaymentValidate, async () => {
-      return 123
+  const tmnCode = config.get('vnp_TmnCode');
+  const secretKey = config.get('vnp_HashSecret');
+  const vnpUrl = config.get('vnp_Url');
+  const returnUrl = config.get('vnp_ReturnUrl');
 
-    })
-  } catch (error) {
+  const date = new Date();
+  const createDate = dateFormat(date, 'yyyymmddHHmmss');
+  const orderId = dateFormat(date, 'HHmmss');
+  const amount = req.body.amount;
+  const bankCode = req.body.bankCode;
 
-    console.log(error);
-
-    return sendResponse(res, 500, "Đã có lỗi xảy ra khi thanh toán");
+  const orderInfo = req.body.orderDescription;
+  const orderType = req.body.orderType;
+  let locale = req.body.language;
+  if (!locale || locale === '') {
+    locale = 'vn';
   }
+  const currCode = 'VND';
+
+  const vnp_Params = {
+    vnp_Version: '2.1.0',
+    vnp_Command: 'pay',
+    vnp_TmnCode: tmnCode,
+    vnp_Locale: locale,
+    vnp_CurrCode: currCode,
+    vnp_TxnRef: orderId,
+    vnp_OrderInfo: orderInfo,
+    vnp_OrderType: orderType,
+    vnp_Amount: amount * 100,
+    vnp_ReturnUrl: returnUrl,
+    vnp_IpAddr: ipAddr,
+    vnp_CreateDate: createDate,
+  };
+
+  if (bankCode) {
+    vnp_Params['vnp_BankCode'] = bankCode;
+  }
+
+  const sortedParams = Object.keys(vnp_Params)
+    .sort()
+    .reduce((acc, key) => {
+      acc[key] = vnp_Params[key];
+      return acc;
+    }, {});
+
+  const signData = qs.stringify(sortedParams, { encode: false });
+  const hmac = crypto.createHmac('sha512', secretKey);
+  const signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+
+  vnp_Params['vnp_SecureHash'] = signed;
+  const redirectUrl = `${vnpUrl}?${qs.stringify(vnp_Params, { encode: false })}`;
+
+  res.redirect(redirectUrl);
 };
 
-export const getAll = async (req, res) => {
+// khi thanh toán thành công hoặc thất bại
+export const vnpayCallback = async (req, res) => {
   try {
-    const paymentList = await PaymentModel.find();
+    const query = req.query;
+    const isValid = vnp.verifySignature(query);
 
-    if (!paymentList || paymentList.length === 0) {
-      return sendResponse(res, 404, "Không có danh sách thanh toán");
+    if (isValid) {
+      // Xử lý đơn hàng thành công
+      const orderId = query.vnp_TxnRef;
+      const amount = query.vnp_Amount;
+
+      // Lưu vào MongoDB
+      const order = new PaymentModel({
+        amount: amount,
+        paymentStatus: 'Thành công', // Hoặc trạng thái tương ứng
+        // Các trường khác của đơn hàng
+        ...req.body
+      });
+
+      await order.save();
+
+      // Gửi email xác nhận
+      const emailOptions = {
+        to: 'recipient@example.com', // Địa chỉ email của người nhận
+        subject: 'Xác nhận thanh toán',
+        text: `Thanh toán thành công cho đơn hàng ${orderId}.`,
+      };
+
+      await sendEmail(emailOptions);
+
+      // Trả về phản hồi cho người dùng
+      sendResponse(res, 200, 'Payment success');
+    } else {
+      // Xử lý đơn hàng không hợp lệ
+      sendResponse(res, 400, 'Invalid payment');
     }
-
-    return sendResponse(res, 200, "Danh sách thanh toán", paymentList);
   } catch (error) {
-    console.error(error);
-
-    return sendResponse(
-      res,
-      500,
-      "Đã có lỗi xảy ra khi lấy danh sách thanh toán"
-    );
+    console.error('Error processing VNPAY callback:', error);
+    sendResponse(res, 500, 'Internal Server Error');
   }
 };
 
-// export const getOne = async (req, res) => {
-//   try {
-//     const payment = await PaymentModel.findById(req.params.id);
-
-//     if (!payment || payment.length === 0) {
-//       return sendResponse(res, 404, "Không có thông tin thanh toán");
-//     }
-
-//     return sendResponse(res, 200, "Thông tin thanh toán", payment);
-//   } catch (error) {
-//     console.error(error);
-
-//     return sendResponse(
-//       res,
-//       500,
-//       "Đã có lỗi xảy ra khi lấy thông tin thanh toán"
-//     );
-//   }
-// };
-
-
-
-// export const update = async (req, res) => {
-//   try {
-//     validateMiddleware(req, res, PaymentValidate, async () => {
-//       const data = await PaymentModel.findByIdAndUpdate(
-//         req.params.id,
-//         req.body,
-//         {
-//           new: true,
-//         }
-//       );
-
-//       if (!data) {
-//         return sendResponse(res, 404, "Cập nhật thanh toán thất bại");
-//       }
-
-//       return sendResponse(res, 200, "Cập nhật thanh toán thành công", data);
-//     });
-//   } catch (error) {
-//     console.log(error);
-
-//     return sendResponse(res, 500, "Đã có lỗi xảy ra khi cập nhật thanh toán");
-//   }
-// };
--- BE/src/router/payment.js
+++ BE/src/router/payment.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { getAll, create } from "../controller/payment";
+import { createPaymentUrl } from "../controller/payment";
 import { isAdminMiddleware } from "../middleware";
 import { loginMiddleware } from "../middleware";
 const router = express.Router();
 
-router.get("/", loginMiddleware, getAll);
-router.post("/", isAdminMiddleware, create);
+// router.get("/", loginMiddleware, getAll);
+router.post("/", loginMiddleware, createPaymentUrl);
 
 export default router;
