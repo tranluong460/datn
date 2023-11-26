1bf6ed3000199d6ff07524871037e3ee040ab060
phucquyet1202 <phucquyet1222003@gmail.com>
2023-11-26 16:40:59 +0700

    feat:[BE] Booking refs: DTN-23

--- BE/src/controller/payment.js
+++ BE/src/controller/payment.js
@@ -15,66 +15,90 @@ import dateFormat from 'dateformat';
  * và chuyển hướng người dùng đến cổng thanh toán VNPAY để thực hiện thanh toán
  */
 export const createPaymentUrl = (req, res) => {
-  const ipAddr =
-    req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
+  try {
+    // Kiểm tra thông tin đầu vào
+    const amount = parseFloat(req.body.amount);
+    if (isNaN(amount) || amount <= 0) {
+      return res.status(400).json({ error: 'Số tiền không hợp lệ' });
+    }
 
-  const tmnCode = config.get('vnp_TmnCode');
-  const secretKey = config.get('vnp_HashSecret');
-  const vnpUrl = config.get('vnp_Url');
-  const returnUrl = config.get('vnp_ReturnUrl');
+    const bankCode = req.body.bankCode || '';
+    const orderInfo = req.body.orderDescription || '';
+    const orderType = req.body.orderType || '';
+    let locale = req.body.language || 'vn';
 
-  const date = new Date();
-  const createDate = dateFormat(date, 'yyyymmddHHmmss');
-  const orderId = dateFormat(date, 'HHmmss');
-  const amount = req.body.amount;
-  const bankCode = req.body.bankCode;
+    // Chuyển đổi giá trị số tiền
+    const vnp_Amount = amount * 100;
 
-  const orderInfo = req.body.orderDescription;
-  const orderType = req.body.orderType;
-  let locale = req.body.language;
-  if (!locale || locale === '') {
-    locale = 'vn';
+    const ipAddr =
+      req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    const tmnCode = 'WMVBW0IU';
+    const secretKey = 'GAFTZWCBLKWZEZTBZCOOQEZHWAVCLNHS';
+    const vnpUrl = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
+    const returnUrl = 'http://localhost:5173/';
+
+    const date = new Date();
+    const createDate = dateFormat(date, 'yyyymmddHHmmss');
+    const orderId = dateFormat(date, 'HHmmss');
+
+    const vnp_Params = {
+      vnp_Version: '2.1.0',
+      vnp_Command: 'pay',
+      vnp_TmnCode: tmnCode,
+      vnp_Locale: locale,
+      vnp_CurrCode: 'VND',
+      vnp_TxnRef: orderId,
+      vnp_OrderInfo: orderInfo,
+      vnp_OrderType: orderType,
+      vnp_Amount: vnp_Amount,
+      vnp_ReturnUrl: returnUrl,
+      vnp_IpAddr: ipAddr,
+      vnp_CreateDate: createDate,
+    };
+
+    // Kiểm tra và thêm các giá trị không bắt buộc
+    if (bankCode) {
+      vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    const sortedParams = Object.keys(vnp_Params)
+      .sort()
+      .reduce((acc, key) => {
+        acc[key] = vnp_Params[key];
+        return acc;
+      }, {});
+
+    const signData = Object.keys(sortedParams)
+      .map((key) => `${key}=${sortedParams[key]}`)
+      .join('&');
+
+    // Sửa chữ ký ở đây, sử dụng SHA-512 và khóa bí mật
+    const hmac = crypto.createHmac('sha512', secretKey);
+    const signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+    vnp_Params['vnp_SecureHash'] = signed;
+
+    // Kiểm tra tính hợp lệ của chữ ký
+    if (signed == vnp_Params.vnp_SecureHash) {
+
+      // Nếu chữ ký hợp lệ, thực hiện chuyển trang
+      const redirectUrl = `${vnpUrl}?${qs.stringify(vnp_Params, { encode: false })}`;
+      // console.log(redirectUrl);
+      res.redirect(redirectUrl);
+    } else {
+      // Nếu chữ ký không hợp lệ, trả về lỗi
+      res.status(400).json({ error: 'Chữ ký không hợp lệ' });
+    }
+  } catch (error) {
+    console.error('Lỗi tạo URL thanh toán:', error);
+    res.status(500).json({ error: 'Lỗi Nội bộ của Server' });
   }
-  const currCode = 'VND';
-
-  const vnp_Params = {
-    vnp_Version: '2.1.0',
-    vnp_Command: 'pay',
-    vnp_TmnCode: tmnCode,
-    vnp_Locale: locale,
-    vnp_CurrCode: currCode,
-    vnp_TxnRef: orderId,
-    vnp_OrderInfo: orderInfo,
-    vnp_OrderType: orderType,
-    vnp_Amount: amount * 100,
-    vnp_ReturnUrl: returnUrl,
-    vnp_IpAddr: ipAddr,
-    vnp_CreateDate: createDate,
-  };
-
-  if (bankCode) {
-    vnp_Params['vnp_BankCode'] = bankCode;
-  }
-
-  const sortedParams = Object.keys(vnp_Params)
-    .sort()
-    .reduce((acc, key) => {
-      acc[key] = vnp_Params[key];
-      return acc;
-    }, {});
-
-  const signData = qs.stringify(sortedParams, { encode: false });
-  const hmac = crypto.createHmac('sha512', secretKey);
-  const signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
-
-  vnp_Params['vnp_SecureHash'] = signed;
-  const redirectUrl = `${vnpUrl}?${qs.stringify(vnp_Params, { encode: false })}`;
-
-  res.redirect(redirectUrl);
 };
+
+
 
 // khi thanh toán thành công hoặc thất bại
 export const vnpayCallback = async (req, res) => {
@@ -98,16 +122,16 @@ export const vnpayCallback = async (req, res) => {
       await order.save();
 
       // Gửi email xác nhận
-      const emailOptions = {
-        to: 'recipient@example.com', // Địa chỉ email của người nhận
-        subject: 'Xác nhận thanh toán',
-        text: `Thanh toán thành công cho đơn hàng ${orderId}.`,
-      };
+      // const emailOptions = {
+      //   to: 'recipient@example.com', // Địa chỉ email của người nhận
+      //   subject: 'Xác nhận thanh toán',
+      //   text: `Thanh toán thành công cho đơn hàng ${orderId}.`,
+      // };
 
-      await sendEmail(emailOptions);
+      // await sendEmail(emailOptions);
 
       // Trả về phản hồi cho người dùng
-      sendResponse(res, 200, 'Payment success');
+      sendResponse(res, 200, 'Payment success', order);
     } else {
       // Xử lý đơn hàng không hợp lệ
       sendResponse(res, 400, 'Invalid payment');
--- BE/src/router/payment.js
+++ BE/src/router/payment.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { createPaymentUrl } from "../controller/payment";
+import { createPaymentUrl, vnpayCallback } from "../controller/payment";
 import { isAdminMiddleware } from "../middleware";
 import { loginMiddleware } from "../middleware";
 const router = express.Router();
 
 // router.get("/", loginMiddleware, getAll);
 router.post("/", loginMiddleware, createPaymentUrl);
+router.post("/vnpay-callback", loginMiddleware, vnpayCallback);
 
 export default router;
