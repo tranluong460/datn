c254dfd8d2fdc41be431c956fa858b4aea7302e0
ducnam2003 <namndph24016@fpt.edu.vn>
2024-01-13 23:10:57 +0700

    up

--- BE/src/controller/review.js
+++ BE/src/controller/review.js
@@ -85,12 +85,15 @@ export const remove = async (req, res) => {
       return sendResponse(res, 400, "ID không hợp lệ");
     }
 
-    const data = await ReviewModel.findById(req.params.id);
+    const data = await ReviewModel.findById(req.params.id).populate({
+      path: "id_user",
+      select: "_id",
+    });
 
-
-    if (data.id_user._id !== req.user._id) {
-      return sendResponse(res, 404, "Không được xóa bình luận vì comment không phải của bạn");
+    if (req.user._id != data.id_user._id.toString()) {
+      return sendResponse(res, 403, "Không có quyền xóa đánh giá");
     }
+
 
     await ReviewModel.findByIdAndDelete(data._id);
     await HotelModel.findOneAndUpdate(
