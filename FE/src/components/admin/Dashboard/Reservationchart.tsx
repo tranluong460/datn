dfeb3b3c437b2f28d39df417a3611e267406e8e9
phucquyet1202 <phucquyet1222003@gmail.com>
2024-01-15 20:46:18 +0700

    up

--- FE/src/components/admin/Dashboard/Reservationchart.tsx
+++ FE/src/components/admin/Dashboard/Reservationchart.tsx
@@ -10,73 +10,13 @@ import {
   YAxis,
 } from "recharts";
 import { useGetAllBookingQuery } from "../../../api";
-
-//   {
-//     name: "Tháng 1",
-//     DoanhThu: 4000,
-//     EmptyRoom: 2400,
-//   },
-//   {
-//     name: "Tháng 2",
-//     DoanhThu: 3000,
-//     EmptyRoom: 1398,
-//   },
-//   {
-//     name: "Tháng 3",
-//     DoanhThu: 2000,
-//     EmptyRoom: 9800,
-//   },
-//   {
-//     name: "Tháng 4",
-//     DoanhThu: 2780,
-//     EmptyRoom: 3908,
-//   },
-//   {
-//     name: "Tháng 5",
-//     DoanhThu: 1890,
-//     EmptyRoom: 4800,
-//   },
-//   {
-//     name: "Tháng 6",
-//     DoanhThu: 2780,
-//     EmptyRoom: 3908,
-//   },
-//   {
-//     name: "Tháng 7",
-//     DoanhThu: 3490,
-//     EmptyRoom: 4300,
-//   },
-//   {
-//     name: "Tháng 8",
-//     DoanhThu: 2000,
-//     EmptyRoom: 9800,
-//   },
-//   {
-//     name: "Tháng 9",
-//     DoanhThu: 4000,
-//     EmptyRoom: 2400,
-//   },
-//   {
-//     name: "Tháng 10",
-//     DoanhThu: 4000,
-//     EmptyRoom: 2400,
-//   },
-//   {
-//     name: "Tháng 11",
-//     DoanhThu: 4000,
-//     EmptyRoom: 2400,
-//   },
-//   {
-//     name: "Tháng 12",
-//     DoanhThu: 3490,
-//     EmptyRoom: 4300,
-//   },
-// ];
+import { DatePicker } from "antd";
 
 function TransactionChart() {
   const { data } = useGetAllBookingQuery('');
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedYear, setSelectedYear] = useState(null);
   const [chartData, setChartData] = useState([]);
+
   const handleSearch = () => {
     const paymentData = data?.data;
 
@@ -84,7 +24,7 @@ function TransactionChart() {
       const filteredPayment = paymentData.filter(
         (booking: any) =>
           booking.status === "Thành Công" &&
-          new Date(booking.createdAt).getFullYear() === selectedYear
+          new Date(booking.createdAt).getFullYear() == selectedYear?.$y
       );
 
       const totalRevenueByMonth = filteredPayment.reduce((acc: any, curr: any) => {
@@ -102,8 +42,6 @@ function TransactionChart() {
     }
   };
 
-  // const chartData = calculateTotalRevenueByMonth();
-  console.log(chartData);
   return (
     <div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1 mb-3">
       <strong className="text-gray-700 font-medium">
@@ -114,36 +52,42 @@ function TransactionChart() {
           <label htmlFor="yearInput" className="mr-2">
             Năm:
           </label>
-          <input
-            type="number"
+          <DatePicker
+            picker="year"
             id="yearInput"
             value={selectedYear}
-            onChange={(e) => setSelectedYear(Number(e.target.value))}
+            onChange={(date) => setSelectedYear(date)}
           />
           <button onClick={handleSearch} className="ml-2 bg-blue-400 text-white p-1">
             Tìm kiếm
           </button>
         </div>
-        {chartData?.length > 0 ? (<ResponsiveContainer width="100%" height={300}>
-          <BarChart
-            width={500}
-            height={300}
-            data={chartData}
-            margin={{
-              top: 20,
-              right: 10,
-              left: 20,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="DoanhThu" fill="#0ea5e9" />
-          </BarChart>
-        </ResponsiveContainer>) : (<div className="font-medium text-center text-xl mt-3"><h2>Không có dữ liệu</h2></div>)}
+        {chartData?.length > 0 ? (
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart
+              width={500}
+              height={300}
+              data={chartData}
+              margin={{
+                top: 20,
+                right: 10,
+                left: 20,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="DoanhThu" fill="#0ea5e9" />
+            </BarChart>
+          </ResponsiveContainer>
+        ) : (
+          <div className="font-medium text-center text-xl mt-3">
+            <h2>Không có dữ liệu</h2>
+          </div>
+        )}
       </div>
     </div>
   );
