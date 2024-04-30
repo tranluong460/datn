3a0ad596cfbfca29ddc1d3828f8c231ce447d6ec
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:33:38 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5399,14 +5399,4 @@ export default GeneralTerms;
 //                   }}
 //                   width="213"
 //                 >
-//                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "justify",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     
+//                  
