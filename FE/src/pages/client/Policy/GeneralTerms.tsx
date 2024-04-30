0ba5912fdbd8124dbbf0cf6f0cf417fcbab0d17a
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:30:52 +0700

    up fix

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5522,18 +5522,4 @@ export default GeneralTerms;
 //                   width="192"
 //                 >
 //                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "justify",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     Từ 12:00 giờ tới trước 15:00 giờ
-//                   </p>
-//                 </td>
-//                 <td
-//                   className="border-divideLight dark:border-divideDark"
-//                   
+//                    
