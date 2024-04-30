bb18097b7eebefa72ccc27e66c2ea8cbfe74aa72
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:34:31 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5374,17 +5374,4 @@ export default GeneralTerms;
 //                     padding: "0in 5.4pt",
 //                   }}
 //                   width="192"
-//                 >
-//                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "justify",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     Sau 18:00 giờ
-//                   </p>
-//                 
+//               
