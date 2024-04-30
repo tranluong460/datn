c08780d25abf6f4515896877dc849602c8128b22
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:36:54 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5302,20 +5302,4 @@ export default GeneralTerms;
 //                   }}
 //                   width="180"
 //                 >
-//                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "center",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     <strong>Nhận phòng sớm</strong>
-//                   </p>
-//                 </td>
-//                 <td
-//                   className="border-divideLight dark:border-divideDark"
-//                   style={{
-//                   
+//                
