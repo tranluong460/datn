4afb11fe03fe0df9956e9125bb0a0fea4200fa4e
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:38:14 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5253,17 +5253,4 @@ export default GeneralTerms;
 //                   }}
 //                   width="371"
 //                 >
-//                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "center",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     <strong>Chính sách</strong>
-//                   </p>
-//                 </td>
-//                 
+//                  
