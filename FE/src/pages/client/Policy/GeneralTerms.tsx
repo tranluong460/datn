e8c3d69ca7928a347c7adf254ceda5ddcdf21164
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:32:40 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5440,21 +5440,4 @@ export default GeneralTerms;
 //                 <td
 //                   className="border-divideLight dark:border-divideDark"
 //                   style={{
-//                     borderWidth: "1px",
-//                     width: "143.7pt",
-//                     borderTop: "none",
-//                     borderLeft: "none",
-//                     padding: "0in 5.4pt",
-//                   }}
-//                   width="192"
-//                 >
-//                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "justify",
-//                       margin: "0in 0in 10pt",
-//                     }}
 //                   
