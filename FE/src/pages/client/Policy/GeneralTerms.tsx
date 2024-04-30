94b5b49c223b12102b3172129e55c488908f8b5d
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-04-30 10:37:34 +0700

    up

--- FE/src/pages/client/Policy/GeneralTerms.tsx
+++ FE/src/pages/client/Policy/GeneralTerms.tsx
@@ -5278,17 +5278,4 @@ export default GeneralTerms;
 //                   width="213"
 //                 >
 //                   <p
-//                     style={{
-//                       marginTop: "6.0pt",
-//                       marginRight: "0in",
-//                       marginBottom: "6.0pt",
-//                       marginLeft: "0in",
-//                       textAlign: "center",
-//                       margin: "0in 0in 10pt",
-//                     }}
-//                   >
-//                     <strong>Phụ thu</strong>
-//                   </p>
-//                 </td>
-//               </tr>
-//              
+//                  
